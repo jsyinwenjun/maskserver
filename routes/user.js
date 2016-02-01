701602d4edb7bdf572abec95ca24151f9513c358
Rail <rail@RaildeiMac.local>
2016-02-01 11:41:27 +0800

    commit

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,13 @@
 
 
-var jwt      	= require('jsonwebtoken');
-var User     	= require('../models/User');
-var formidable 	= require('formidable');
-var fs 			= require('fs');
+var jwt         = require('jsonwebtoken');
+var User        = require('../models/User');
+var formidable  = require('formidable');
+var fs          = require('fs');
 
 exports.signin = function(req, res) {
 
-	User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
+    User.findOne({email: req.body.email, password: req.body.password}, function(err, user) {
         if (err) {
             res.json({
                 success: false,
@@ -15,14 +15,14 @@ exports.signin = function(req, res) {
             });
         } else {
             if (user) {
-            	user.token = jwt.sign(user.email, process.env.JWT_SECRET);
-            	user.lastLoginDate = new Date();
-            	user.save(function(err, user1) {
+                user.token = jwt.sign(user.email, process.env.JWT_SECRET);
+                user.lastLoginDate = new Date();
+                user.save(function(err, user1) {
                         res.json({
                             success: true,
                             user: {email:user1.email, 
-                            	password: user1.password,
-                            	photo:user1.photo
+                                password: user1.password,
+                                photo:user1.photo
                             },
                             token: user1.token
                         });
@@ -65,7 +65,7 @@ exports.signup = function (req, res) {
                         res.json({
                             success: true,
                             user: {email:user1.email, 
-                            	password: user1.password
+                                password: user1.password
                             },
                             token: user1.token
                         });
@@ -94,14 +94,14 @@ exports.me = function(req, res) {
 };
 
 exports.checkToken = function(req, res, next) {
-	User.findOne({token: req.token}, function(err, user) {
+    User.findOne({token: req.token}, function(err, user) {
         if (err) {
             res.json({
                 success: false,
                 message: "Error occured: " + err
             });
         } else {
-        	req.user = user;
+            req.user = user;
             next();
         }
     });
@@ -109,33 +109,35 @@ exports.checkToken = function(req, res, next) {
 
 
 exports.upload = function(req, res) {
-	var form = new formidable.IncomingForm();
-	form.encoding = 'utf-8';
+    var form = new formidable.IncomingForm();
+    form.encoding = 'utf-8';
     form.uploadDir = process.env.USER_PHOTO_TEMP_DIR;
     form.keepExtensions = true;
     form.maxFieldsSize = 2 * 1024 * 1024;
 
     form.parse(req, function(err, fields, files) {
-		if (err) {
-	      res.json({
-	      	success: false,
-	      	message: "Error occured: " + err
-	      });
-	      return;		
-	    }
-	    var extName = fields.name.match(/.[\w+]+$/);
-	    if (req.user.photo) {
-	    	fs.unlink(process.env.USER_PHOTO_DIR + '/' + req.user.photo);
-	    }
+        if (err) {
+          res.json({
+            success: false,
+            message: "Error occured: " + err
+          });
+          return;       
+        }  
+         
+        var extName = fields.name.match(/.[\w+]+$/);
 
-	    var avatarName = req.user.email + Date.now() + extName;
-	    var newPath = process.env.USER_PHOTO_DIR + '/' + avatarName;
-	    fs.rename(files.content.path,newPath);
-	    console.log("file upload success at: " + newPath);
-		console.log("file size: " + files.content.size);
-	    req.user.photo = avatarName;
-	    req.user.updateDate = new Date();
-	    req.user.save(function(err, user) {
+        if (req.user.photo) {
+            fs.unlink(process.env.USER_PHOTO_DIR + '/' + req.user.photo);
+        }
+
+        var avatarName = req.user.email + Date.now() + extName;
+        var newPath = process.env.USER_PHOTO_DIR + '/' + avatarName;
+        fs.rename(files.content.path,newPath);
+        console.log("file upload success at: " + newPath);
+        console.log("file size: " + files.content.size);
+        req.user.photo = avatarName;
+        req.user.updateDate = new Date();
+        req.user.save(function(err, user) {
 
             res.json({
                 success: true,
@@ -150,78 +152,82 @@ exports.upload = function(req, res) {
 
 
 exports.photo = function(req, res) {
-	User.findOne({email: req.query.email}, function(err, user) {
+    User.findOne({email: req.query.email}, function(err, user) {
         if (err) {
             res.json({
                 success: false,
                 message: "Error occured: " + err
             });
         } else if (user){
-        	if (user.photo) {
-        		res.json({
-	        		success: true,
-	        		photo: user.photo
-	        	});
-        	}else {
-        		res.json({
-	        		success: false,
-	        		message: "photo not exist"
-	        	});
+            if (user.photo) {
+                res.json({
+                    success: true,
+                    photo: user.photo
+                });
+            }else {
+                res.json({
+                    success: false,
+                    message: "photo not exist"
+                });
 
-        	}
-        	
+            }
+            
         }else {
-        	res.json({
-        		success: false,
-        		message: "user not exist"
-        	});
+            res.json({
+                success: false,
+                message: "user not exist"
+            });
         }
     });
 
 }
 
 exports.update = function(req,  res) {
-	if (req.body.birthday != undefined) {
-		req.user.birthday = req.body.birthday;
-	}
+    if (req.body.birthday != undefined) {
+        req.user.birthday = req.body.birthday;
+    }
 
-	if (req.body.gender != undefined) {
-		req.user.gender   = req.body.gender;
-	}
-	
-	if (req.body.skinOily != undefined) {
-		req.user.skinOily = req.body.skinOily;
-	}
+    if (req.body.gender != undefined) {
+        req.user.gender   = req.body.gender;
+    }
+    
+    if (req.body.skinOily != undefined) {
+        req.user.skinOily = req.body.skinOily;
+    }
 
-	if (req.body.skinNormal != undefined) {
-		req.user.skinNormal = req.body.skinNormal;
-	}
+    if (req.body.skinNormal != undefined) {
+        req.user.skinNormal = req.body.skinNormal;
+    }
 
-	if (req.body.skinDry != undefined) {
-		req.user.skinDry = req.body.skinDry;
-	}
+    if (req.body.skinDry != undefined) {
+        req.user.skinDry = req.body.skinDry;
+    }
 
-	if (req.body.skinSensitive != undefined) {
-		req.user.skinSensitive = req.body.skinSensitive;
-	}
+    if (req.body.skinSensitive != undefined) {
+        req.user.skinSensitive = req.body.skinSensitive;
+    }
 
-	if (req.body.skinCareTime != undefined) {
-		req.user.skinCareTime = req.body.skinCareTime;
-	}
+    if (req.body.skinCareTime != undefined) {
+        req.user.skinCareTime = req.body.skinCareTime;
+    }
 
-	req.updateDate = new Date();
-	req.user.save(function(err, user){
-		if (err) {
-			res.json({
+    if (req.body.weekPlan != undefined) {
+        req.user.weekPlan = req.body.weekPlan;
+    }
+
+    req.updateDate = new Date();
+    req.user.save(function(err, user){
+        if (err) {
+            res.json({
                 success: false,
                 message: "Error occured: " + err
             });
-		}else {
-			res.json({
+        }else {
+            res.json({
                 success: true
             });
-		}
-	});
+        }
+    });
 
 }
 
